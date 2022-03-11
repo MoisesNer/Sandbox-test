3d79a12e733e4167621d4e81cc754efa95b7169f
MoisesNer <70537402+MoisesNer@users.noreply.github.com>
2022-03-11 10:32:30 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="Header">HEADER</div>
+        <div className="body">loremx10</div>
+        <div className="side">SIDE</div>
+        <div className="footer">FOOTER</div>
+      </div>
+    </div>
+  );
+}
